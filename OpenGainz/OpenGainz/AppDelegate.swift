3d350161697e9a4af3d90f8a42479f3a345fd0cd
fuego-amigo <fuego_amigo@protonmail.com>
2018-02-25 16:51:45 +0800

    Inhibit pod warnings

--- OpenGainz/OpenGainz/AppDelegate.swift
+++ OpenGainz/OpenGainz/AppDelegate.swift
@@ -7,16 +7,28 @@
 //
 
 import Firebase
+import FirebaseAuthUI
 import UIKit
 
 @UIApplicationMain
-internal class AppDelegate: UIResponder, UIApplicationDelegate {
+internal class AppDelegate: UIResponder, UIApplicationDelegate, FUIAuthDelegate {
   internal var window: UIWindow?
   
   internal func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
+    
+    let firebaseAuthUI = FUIAuth.defaultAuthUI()
+    firebaseAuthUI?.delegate = self
     return true
   }
+  
+  internal func authUI(
+    _ authUI: FUIAuth,
+    didSignInWith user: User?,
+    error: Error?) {
+    
+  }
+  
 }
