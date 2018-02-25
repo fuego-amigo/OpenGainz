9ee7cd95567d2f1a60e075c44bfb15cbb06df010
fuego-amigo <fuego_amigo@protonmail.com>
2018-02-25 21:19:20 +0800

    Add Auth providers

--- OpenGainz/OpenGainz/AppDelegate.swift
+++ OpenGainz/OpenGainz/AppDelegate.swift
@@ -8,27 +8,41 @@
 
 import Firebase
 import FirebaseAuthUI
+import FirebaseGoogleAuthUI
 import UIKit
 
 @UIApplicationMain
 internal class AppDelegate: UIResponder, UIApplicationDelegate, FUIAuthDelegate {
   internal var window: UIWindow?
   
+  internal var firebaseAuthUI = FUIAuth.defaultAuthUI()
+
   internal func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
-    
-    let firebaseAuthUI = FUIAuth.defaultAuthUI()
+
     firebaseAuthUI?.delegate = self
+    firebaseAuthUI?.providers = [FUIGoogleAuth()]
+
+    let authViewController = firebaseAuthUI?.authViewController()
     return true
   }
   
+  func application(
+    _ app: UIApplication, open url: URL,
+    options: [UIApplicationOpenURLOptionsKey: Any]) -> Bool {
+    let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?
+    if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+      return true
+    }
+    return false
+  }
+
   internal func authUI(
     _ authUI: FUIAuth,
     didSignInWith user: User?,
     error: Error?) {
     
   }
-  
 }
